3959e297791d8143c11b3054f9c2181dfa1e03b8
Shakhzod Bobolov <shohbobolov98@gmail.com>
2024-12-02 20:11:31 +0500

    feat: url search params

--- react/src/pages/home/components/homeSection/homeSection.jsx
+++ react/src/pages/home/components/homeSection/homeSection.jsx
@@ -1,16 +1,36 @@
-import { Link } from "react-router-dom";
-import { useState } from "react";
+import {
+  Link,
+  useLocation,
+  useParams,
+  useSearchParams,
+} from "react-router-dom";
 import { Box } from "../box";
 
+const data = [
+  {
+    id: 1,
+    name: "Macarons (12pcs)",
+    price: 15.0,
+    image:
+      "https://cdn.pixabay.com/photo/2017/05/23/17/12/macarons-2338463_960_720.jpg",
+    category: "Croissant",
+    rating: 4,
+  },
+];
+
 export const HomeSection = () => {
+  const [params] = useSearchParams();
+  const category = params.get("q");
   return (
     <>
       <div className="w-full flex flex-col">
         <div className="flex w-full justify-evenly max-w-[1092px] mt-[30px] mx-auto mb-[40px]">
-          <div className="flex items-center hover:border-b-2 hover:border-[#996531] transition-all duration-500 ease-in-out border-b-2 border-transparent hover:text-[#996531] cursor-pointer">
-            <i className="fa-solid fa-fire"></i>
-            <p>Most popular</p>
-          </div>
+          <Link to={"/home?q=popular"}>
+            <div className="flex items-center hover:border-b-2 hover:border-[#996531] transition-all duration-500 ease-in-out border-b-2 border-transparent hover:text-[#996531] cursor-pointer">
+              <i className="fa-solid fa-fire"></i>
+              <p>Most popular</p>
+            </div>
+          </Link>
           <div className="cursor-pointer hover:border-b-2 hover:border-[#996531] transition-all duration-500 ease-in-out border-b-2 border-transparent hover:text-[#996531] flex items-center text-[#1C140C]">
             <i className="fa-regular fa-heart"></i>
             <p>Favorites</p>
@@ -49,9 +69,11 @@ export const HomeSection = () => {
           </div>
         </div>
         <div className="w-full gap-[20px] flex flex-wrap justify-center max-w-[1100px] mx-auto overflow-y-scroll max-h-[78.2vh]">
-          {Array.from({ length: 30 }).map((_, index) => (
-            <Box key={index} />
-          ))}
+          {data
+            .filter((item) => item.category === category)
+            .map((_, index) => (
+              <Box key={index} />
+            ))}
         </div>
       </div>
     </>
